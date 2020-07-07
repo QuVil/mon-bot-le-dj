10e54ee8384a7201abb2dc410ed29725656b4e43
Gary SUBLET <gary.sublet@hotmail.fr>
2020-07-07 19:40:00 +0200

    set the api column to the ids of spotify

--- src/ach.py
+++ src/ach.py
@@ -8,6 +8,7 @@ CREDENTIALS_PATH_GOOGLE = 'google-credentials.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET = '1nkczsf-EeEWVzoNjD3UdB2Xr2W4jD7YEXD_ImRbXHNc'  # GARY SHEETS
 ACH_SHEETS = "achmusik.pkl"
+API_PREFIX = "api:"
 
 
 class Ach:
@@ -15,7 +16,28 @@ class Ach:
     def __init__(self):
         create_cache_dir()
 
+    def __column_to_letter(self, idx):
+        character = chr(ord('A') + idx % 26)
+        remainder = idx // 26
+        if idx >= 26:
+            return self.__column_to_letter(remainder-1) + character
+        else:
+            return character
+
+    def __get_api_columns(self, headers):
+        api_col = {}
+        for idx, name in enumerate(headers):
+            if API_PREFIX in name:
+                api_col[name] = self.__column_to_letter(idx)
+        self.api_columns = api_col
+        print(self.api_columns)
+
+    def __drop_api_columns(self, ach):
+        return ach.drop(self.api_columns, axis=1)
+
     def __load_from_cache(self):
+        print("Error while reading from google\n"
+              "Reading from cache")
         return pd.read_pickle(cache(ACH_SHEETS))
 
     def __load_from_google(self):
@@ -24,12 +46,13 @@ class Ach:
             CREDENTIALS_PATH_GOOGLE, scopes=SCOPES)
 
         # Creates Google Sheets API (v4/latest) service.
-        service = build('sheets', 'v4', credentials=credentials)
+        self.service = build('sheets', 'v4', credentials=credentials)
         # Gets values from Ach! Musik: Notations sheet.
-        values = service.spreadsheets().values()\
+        values = self.service.spreadsheets().values()\
             .get(spreadsheetId=SPREADSHEET, range='Notations')\
             .execute()['values']
         headers = values.pop(0)
+        self.__get_api_columns(headers)
         ach = pd.DataFrame.from_records(values)
         if ach.shape[1] > len(headers):
             ach.drop(ach.columns[len(headers):], axis=1, inplace=True)
@@ -37,6 +60,7 @@ class Ach:
         ach.columns = headers
         ach.set_index(['genre', 'sub_genre', 'artist', 'album', 'song'],
                       inplace=True)
+        ach = self.__drop_api_columns(ach)
         return ach
 
     def get_sheets(self):
@@ -47,5 +71,22 @@ class Ach:
             ach = self.__load_from_cache()
         return ach
 
-    def update_missing(self, missing):
+    def update_missing(self, ids):
+        column = self.api_columns["api:Spotify"]
+        range_ = f"Notations!{column}2:{column}{len(ids)+1}"
+        ids_strings = ids.fillna("")
+        payload = {
+            "majorDimension": "ROWS",
+            "values": ids_strings.values.reshape(-1, 1).tolist()
+        }
+        print(range_)
+        print("lol")
+        self.service.spreadsheets()\
+                    .values()\
+                    .update(spreadsheetId=SPREADSHEET,
+                            range=range_,
+                            valueInputOption="RAW",
+                            body=payload)\
+                    .execute()
+
         pass
