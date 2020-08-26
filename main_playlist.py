b675fa399396e9fbb4d0ae1126eef08869a09b3a
Quentin <quevil@msn.com>
2020-08-26 23:00:05 +0200

    Merged successfuly

--- main_playlist.py
+++ main_playlist.py
@@ -20,10 +20,11 @@ if __name__ == "__main__":
 
     # OPTIONAL but better to do
     # update the missing id list from the sheet
-    ach.update_missing(ids, muzik.name)
+    # ach.update_missing(ids, muzik.name)
 
     # generate playlist with only the songs that "exists"
-    playlist = create_playlist(sheet.loc[ids.index])
+    playlist = create_playlist(sheet.loc[ids.index], ["Qu", "Vi", "Sa", "Ro"], count_factor=.4, inhib_factor=2,
+                               min_score=7, size=180, default_grade=5, eliminating_grade=4.6)
 
     # push the playlist
     muzik.create_playlist(playlist)
